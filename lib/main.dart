9405fb89e092fd5e68c3abd1934cddbc7d9e4522
aadesh18 <110230993+aadesh18@users.noreply.github.com>
2024-02-18 10:05:33 -0500

    initial_commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:inzone/firebase_options.dart';
@@ -20,9 +19,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  FirebaseUIAuth.configureProviders([
-    PhoneAuthProvider(),
-  ]);
+  
   await FirebaseMessaging.instance.getInitialMessage();
 
   FirebaseMessaging.onBackgroundMessage(_backgroundMessageHandler);
